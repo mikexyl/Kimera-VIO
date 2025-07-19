704422297c0fc3de043e6f62ee110303951e3ff5
Mike Xiangyu Liu <liu.xiangyu@ucy.ac.cy>
2025-07-19 22:24:22 +0300

    tuning lc parameters

--- include/kimera-vio/loopclosure/VLADLoopClosureDetector.h
+++ include/kimera-vio/loopclosure/VLADLoopClosureDetector.h
@@ -243,7 +243,7 @@ class VLADLoopClosureDetector
     feature_matcher_->match(
         cur_ret, image_size0, ref_ret, image_size0, matches);
 
-    if (matches.size() < 50) {
+    if (matches.size() < 30) {
       LOG(WARNING) << "VLADLoopClosureDetector: LG: Not enough matches found: "
                    << matches.size() << ".";
       return;
