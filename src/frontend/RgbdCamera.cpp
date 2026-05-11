22480ae4e67eb660ca8a036043b989be438c165e
Mike Xiangyu Liu <liu.xiangyu@ucy.ac.cy>
2026-05-11 12:40:19 +0300

    Port SB-SLAM ROS2 support

--- src/frontend/RgbdCamera.cpp
+++ src/frontend/RgbdCamera.cpp
@@ -50,7 +50,7 @@ void convertToPcl(const cv::Mat& intensity_img,
   cv::Mat_<cv::Point3f> cloud_out =
       cv::Mat(img_rows, img_cols, CV_32FC3, cv::Scalar(0.0, 0.0, 0.0));
   cv::Mat colors_out =
-      cv::Mat(img_rows, img_cols, CV_8UC3, cv::viz::Color::red());
+      cv::Mat(img_rows, img_cols, CV_8UC3, cv::Scalar(0, 0, 255));
 
   for (int v = 0u; v < img_rows; ++v) {
     for (int u = 0u; u < img_cols; ++u) {
